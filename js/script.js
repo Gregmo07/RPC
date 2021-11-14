ed4ed5798309cf6b8c9a090ae120374d66fd263b
Gregmo07 <laurentg07@gmail.com>
2021-11-14 23:59:40 +0100

    UI 2

--- js/script.js
+++ js/script.js
@@ -4,20 +4,49 @@ const matrice = [
                     [true, "tie", false],
                     [false, true, "tie"]
                 ];
+                
+
+let humanScore = 0;
+let computerScore = 0;
+let msg;
+const result = document.querySelector('.result'); 
+const score = document.querySelector('.score');
+const scores = document.querySelector('.scores');
 
 function playRound(e) {
-    let msg;
+
+    if (!e.target.id) return;
+
     let humanPlay = e.target.id;
     let computerPlay = Math.floor(Math.random() * 3);
-    const score = matrice[humanPlay][computerPlay];
-    if (score == "tie"){
+    const roundScore = matrice[humanPlay][computerPlay];
+
+    if (roundScore == "tie"){
         msg = `Egalité ! ${play[computerPlay]} = ${play[humanPlay]}`;
-    } else if (score){
+    } else if (roundScore){
         msg = `Vous avez gagné ! ${play[humanPlay]} bat ${play[computerPlay]}`;
+        humanScore += 1;
     } else {
         msg = `Vous avez perdu! ${play[computerPlay]} bat ${play[humanPlay]}`;
+        computerScore += 1;
     }
-    console.log(msg);
+    result.textContent = `${msg} ! `;
+    score.textContent = `${humanScore} - ${computerScore}`;
+
+    
 }  
 
-window.addEventListener('click', playRound);
+function checkScore(humanScore, computerScore) {
+    let gagnant;
+    if (humanScore == 5){
+        gagnant = 'l\'Humain';
+    } else if (computerScore == 5){
+        gagnant = 'la Machine'
+    }
+    
+    const winner =  document.createElement('p');
+    
+       
+}
+
+document.addEventListener('click', playRound);
