82f3bd2fb8f88187a3de7c92912868316c36399b
Gregmo07 <laurentg07@gmail.com>
2021-11-21 23:15:28 +0100

    final

--- js/script.js
+++ js/script.js
@@ -13,13 +13,16 @@ const scores = document.querySelector('.scores');
 const choiceButton = document.getElementsByClassName("key");
 const restartButton = document.querySelector('.restart');
 
+const transition = document.querySelectorAll(".transition");
+
 function playRound(e) {
 
     if (!e.target.id || humanScore == 5 || computerScore == 5) return;
+   
     const buttonClicked = document.querySelector(`button[id="${e.target.id}"]`);
     buttonClicked.classList.add('playing');
     const wordChoose = document.querySelector(`span[id="word${e.target.id}"]`);
-    wordChoose.classList.add('playing');
+    wordChoose.classList.add('wordTransition');
 
     let humanPlay = e.target.id;
     let computerPlay = Math.floor(Math.random() * 3);
@@ -51,7 +54,6 @@ function checkScore(humanScore, computerScore) {
     } else if (computerScore == 5){
         gagnant = 'la Machine'
     }
-    
     const winner =  document.createElement('p');
     winner.setAttribute("id", "winner");
     winner.textContent = `Le vainqueur est : ${gagnant}`;
@@ -71,14 +73,15 @@ function restartGame() {
     winner.remove();
     
 }
+
 function removeTransition(e) {
     if (e.propertyName !== 'transform') return;
     e.target.classList.remove('playing');
+    e.target.classList.remove('wordTransition');
   }
 
-  
-const keys = Array.from(document.querySelectorAll('.key'));
-  keys.forEach(key => key.addEventListener('transitionend', removeTransition)); 
+ 
+transition.forEach(key => key.addEventListener('transitionend', removeTransition)); 
 
 Array.from(choiceButton).forEach(element => {
         element.addEventListener('click', playRound);
